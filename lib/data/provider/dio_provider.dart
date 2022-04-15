9f8692e151341c1bacf1b572b2506405e8c2efed
nigoro <dolessup@gmail.com>
2022-04-15 11:49:58 +0900

    👽️ Personal Access Tokenを設定できるように

--- lib/data/provider/dio_provider.dart
+++ lib/data/provider/dio_provider.dart
@@ -1,4 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final dioProvider = Provider<Dio>((ref) => Dio());
+final dioProvider = Provider<Dio>((ref) {
+  final dio = Dio();
+  const token = String.fromEnvironment('TOKEN');
+  if (token != '') {
+    const authorization = 'token ' + token;
+    dio.options.headers['Authorization'] = authorization;
+  }
+  return dio;
+});
